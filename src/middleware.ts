7d363d21a85a4d8f29e5d0d586609ec495da36c2
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2023-05-13 08:24:39 -0400

    Fixing bug due to x-forwarded-proto being comma-separated list of values and not a single http vs https value

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,11 +7,10 @@ const middleware: NextMiddleware = (request, event) => {
   // https://stackoverflow.com/questions/66458059/  how-do-i-force-or-redirect-my-next-js-website-to-use-https
   if (
     currentEnv === 'production' && 
-    request.headers.get("x-forwarded-proto") !== "https"
+    request.headers.get("X-Forwarded-Proto")?.split(',')[0] !== "https"
   ) {
     console.log('[MIDDLEWARE] Redirecting HTTP traffic to HTTPS');
-    console.log("[MIDDLEWARE] x-forwarded-proto", request.headers.get("x-forwarded-proto"));
-    console.log("[MIDDLEWARE] X-Forwarded-Proto", request.headers.get("X-Forwarded-Proto"));
+    console.log("[MIDDLEWARE] X-Forwarded-Proto value:", request.headers.get("X-Forwarded-Proto"));
       return NextResponse.redirect(
         `https://${request.headers.get('host')}${request.nextUrl.pathname}`,
         301
